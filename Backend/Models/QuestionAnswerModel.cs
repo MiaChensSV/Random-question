1923200b7a751efdea7c553e82eb5d2d7159ff23
MiaChensSV <miachensv@gmail.com>
2023-06-02 19:31:00 +0200

    Added error handler to return json str

--- Backend/Models/QuestionAnswerModel.cs
+++ Backend/Models/QuestionAnswerModel.cs
@@ -1,44 +1,31 @@
-﻿namespace DHA_Code_Test_Backend.Models
+﻿namespace DHA_Code_Test_Backend.Models;
+
+public class QuestionAnswerModel
 {
-	public class QuestionAnswerModel
+	public int QuestionAnswerId { get; set; }
+	public string QuestionText { get; set; } = null!;
+	public string[] Options { set; get; } = null!;
+	public int AnswerIndex { get; set; } = 0;
+
+	public QuestionAnswerModel(
+		int questionAnswerId, 
+		string questionText,
+		string[] options,
+		int answerIndex
+	) { 
+		this.QuestionAnswerId = questionAnswerId;
+		this.QuestionText = questionText;
+		this.Options = options;
+		this.AnswerIndex = answerIndex;
+	}
+
+	public QuestionModel ToQuestionModel()
 	{
-		private string[] _options;
-		public int QuestionAnswerId { get; set; }
-		public string QuestionText { get; set; } = null!;
-		public string[] Options 
-		{
-			get { return this._options; }
-			set 
-			{
-				if (value.Length != 3)
-				{
-					throw new ArgumentException("Number of options must be 3!");
-				}
-				this._options = value;
-			} 
-		}
-		public int AnswerIndex { get; set; } = 0;
+		return new QuestionModel(this.QuestionAnswerId, this.QuestionText, this.Options);
+	}
 
-		public QuestionAnswerModel(
-			int questionAnswerId, 
-			string questionText,
-			string[] options,
-			int answerIndex
-		) { 
-			this.QuestionAnswerId = questionAnswerId;
-			this.QuestionText = questionText;
-			this.Options = options;
-			this.AnswerIndex = answerIndex;
-		}
-
-		public QuestionModel ToQuestionModel()
-		{
-			return new QuestionModel(this.QuestionAnswerId, this.QuestionText, this.Options);
-		}
-
-		public AnswerModel ToAnswerModel()
-		{
-			return new AnswerModel(this.QuestionAnswerId, this.Options, this.AnswerIndex);
-		}
+	public AnswerModel ToAnswerModel()
+	{
+		return new AnswerModel(this.QuestionAnswerId, this.Options, this.AnswerIndex);
 	}
 }
