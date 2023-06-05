f1cc8a1fcad2f41399f8a0a115cf8a02a0dcc02e
MiaChensSV <miachensv@gmail.com>
2023-06-05 14:56:21 +0200

    modified DummyDB

--- Backend/Models/OptionModel.cs
+++ Backend/Models/OptionModel.cs
@@ -1,6 +1,13 @@
-﻿namespace DHA_Code_Test_Backend.Models
+﻿namespace DHA_Code_Test_Backend.Models;
+
+public enum Option { One, X, Two }	
+public class OptionModel
 {
-	public class OptionModel
+	public Option OptionValue { get; set; }
+	public string OptionText { get; set; }
+	public OptionModel(Option optionValue, string optionText)
 	{
+		this.OptionValue = optionValue;
+		this.OptionText = optionText;
 	}
 }
